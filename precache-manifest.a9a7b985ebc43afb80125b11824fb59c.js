1b3e2abc0b8ce88bc9b829e048954d48cee9c743
prabhat1725 <prabhatupadhyay2512@gmail.com>
2020-06-14 14:30:35 +0530

    Updates

---
+++ precache-manifest.a9a7b985ebc43afb80125b11824fb59c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf11e42be6894601c073b2ce7388f02",
+    "url": "/abhyant/index.html"
+  },
+  {
+    "revision": "7237358efb6507197766",
+    "url": "/abhyant/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d6628bc217c8cc4abe03",
+    "url": "/abhyant/static/js/2.7d072569.chunk.js"
+  },
+  {
+    "revision": "7237358efb6507197766",
+    "url": "/abhyant/static/js/main.587af5c2.chunk.js"
+  },
+  {
+    "revision": "b27fa563a243d2e93a97",
+    "url": "/abhyant/static/js/runtime-main.72ef1e00.js"
+  }
+]);
